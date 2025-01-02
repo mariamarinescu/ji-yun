a0c1c69417f24ecab23bb3316f79e541cf55a9a5
Maria Marinescu <mariafmarinescu@outlook.com>
2025-01-02 21:06:47 +0100

    fix: main layout

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -6,7 +6,12 @@ export const Layout = () => {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className={clsx('flex-1 items-start bg-gray-100 p-4')}>
+      <main
+        className={clsx(
+          'flex-1 items-start bg-gray-100 p-4',
+          'mx-auto flex w-full bg-white p-4 text-black xl:max-w-screen-lg'
+        )}
+      >
         <Outlet />
       </main>
       <Footer />
