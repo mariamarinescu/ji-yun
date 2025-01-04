598a62ac0b74dc80ec32b4c71d9f7d88b56e6105
Maria Marinescu <mariafmarinescu@outlook.com>
2025-01-04 14:43:03 +0100

    fix(build): make default param option for Project

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -3,8 +3,7 @@ import { Project as ProjectType } from 'interfaces';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
-export const Project: FC<{ project: ProjectType }> = ({ project }) => {
-  const { id, client, img, tags } = project;
+export const Project: FC<{ project?: ProjectType }> = ({ project }) => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -15,21 +14,21 @@ export const Project: FC<{ project: ProjectType }> = ({ project }) => {
         delay: 0.15,
       }}
     >
-      <Link to={`/ji-yun/projects/${id}`} aria-label="Single Project">
+      <Link to={`/ji-yun/projects/${project?.id}`} aria-label="Single Project">
         <div className="bg-secondary-light dark:bg-ternary-dark mb-10 cursor-pointer rounded-xl shadow-lg hover:shadow-xl sm:mb-0">
           <div>
             <img
-              src={img}
+              src={project?.img}
               className="rounded-t-xl border-none"
               alt="Single Project"
             />
           </div>
           <div className="px-4 py-6 text-center">
             <p className="font-general-medium text-ternary-dark dark:text-ternary-light mb-2 text-lg md:text-xl">
-              {client}
+              {project?.client}
             </p>
             <span className="text-ternary-dark dark:text-ternary-light text-lg">
-              {tags.map((tag) => tag).join(', ')}
+              {[project?.tags || []].map((tag) => tag).join(', ')}
             </span>
           </div>
         </div>
