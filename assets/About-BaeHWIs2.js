d57442c7f8c01f5ba285f62e3ebdda0168dd9f33
mariamarinescu <mariamarinescu@users.noreply.github.com>
2025-01-08 17:27:08 +0000

    deploy: 7df40dde262d60d079e7652c377438db00904836

---
+++ assets/About-BaeHWIs2.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-C7jXLWqk.js";const e=()=>t.jsx("div",{children:t.jsx("h1",{children:"About me"})});export{e as default};
---
+++ assets/Contact-CS2rPfm4.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-C7jXLWqk.js";const n=()=>t.jsx("div",{children:t.jsx("h1",{children:"Contact"})});export{n as default};
