1045eaa56d2e3bbaf2ea31292777cfbce4be16af
Maria Marinescu <mariafmarinescu@outlook.com>
2025-01-10 10:53:08 +0100

    perf(routes): Add Project page as child of Projects

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,13 @@
 import { lazy } from 'react';
 import { Navigate, Route, Routes as RouterRoutes } from 'react-router-dom';
 import { ErrorElement, Layout } from './components';
+import { Project } from './pages';
 import Projects from './pages/Projects';
 
 const basePath = '/ji-yun';
 
 const About = lazy(() => import('./pages/About'));
 const Profile = lazy(() => import('./pages/Profile'));
-const Project = lazy(() => import('./pages/Project'));
 const Contact = lazy(() => import('./pages/Contact'));
 
 export const Routes = () => {
@@ -21,8 +21,12 @@ export const Routes = () => {
         <Route path={basePath} element={<Navigate to="about" replace />} />
         <Route path="about" element={<About />} />
         <Route path="profile" element={<Profile />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="projects/:projectId" element={<Project />} />
+        <Route
+          path="projects"
+          element={<Projects />}
+          children={[<Route path=":projectId" element={<Project />} />]}
+        />
+
         <Route path="contact" element={<Contact />} />
       </Route>
     </RouterRoutes>
