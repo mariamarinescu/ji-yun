531decd3a8425f3f79fd92538afa421e432350c0
Maria Marinescu <mariafmarinescu@outlook.com>
2025-01-11 17:28:42 +0100

    fix(build): remove prefix from route

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: '/ji-yun/',
+    base: '/',
     define: {
       'process.env': env,
     },
