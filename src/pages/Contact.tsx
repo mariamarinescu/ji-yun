b8e8f08d25a2306054ae758bf30f3a23467e5c30
Maria Marinescu <mariafmarinescu@outlook.com>
2025-01-11 17:53:49 +0100

    fix(contact): fix contact page rendering

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,14 @@
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="isolate bg-white py-16">
+      <div className="mx-auto max-w-2xl text-start">
+        <h2 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-2xl">
+          Contact me
+        </h2>
+        <p className="mt-2 text-lg/8 text-gray-600">
+          You can send me an email or contact via social networks.
+        </p>
+      </div>
     </div>
   );
 };
