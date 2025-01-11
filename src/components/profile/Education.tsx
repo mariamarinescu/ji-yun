7a764e428bfe420177f52ba246fc82c77e9ed9e2
Maria Marinescu <mariafmarinescu@outlook.com>
2025-01-11 15:58:10 +0100

    fix: increase Education comp. elements size

--- src/components/profile/Education.tsx
+++ src/components/profile/Education.tsx
@@ -4,16 +4,18 @@ import universityLogo from '../../assets/Musabi.png';
 export const Education = () => {
   return (
     <div className="flex w-full flex-col">
-      <h5 className="mt-2 text-pretty text-xl font-semibold tracking-tight text-gray-900 sm:text-2xl">
+      <h5 className="mt-2 text-pretty text-xl font-semibold tracking-tight text-gray-900">
         Education
       </h5>
       <div className="flex flex-wrap items-center gap-2 py-2">
-        <img src={universityLogo} width="30%" height="" alt="University logo" />
-        <div className="gap flex flex-col items-start">
-          <p className="font-semibold text-black">Musashino Art University</p>
+        <img src={universityLogo} width="20%" height="" alt="University logo" />
+        <div className="flex h-full flex-col">
+          <p className="text-sm font-semibold text-black">
+            Musashino Art University
+          </p>
           <div className="gap flex items-center gap-2">
-            <GrMapLocation size="30" />
-            <p className="font-semibold text-black">Tokyo, Japan</p>
+            <GrMapLocation size="20" />
+            <p className="text-sm font-semibold text-black">Tokyo, Japan</p>
           </div>
         </div>
       </div>
