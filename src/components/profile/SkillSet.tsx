dd216b51b345d4227143940c4ab178951cd3ad83
Maria Marinescu <mariafmarinescu@outlook.com>
2025-01-11 16:05:34 +0100

    refactor: improve SkillSet component logic

--- src/components/profile/SkillSet.tsx
+++ src/components/profile/SkillSet.tsx
@@ -5,40 +5,42 @@ import {
   MicrosoftOfficeIcon,
 } from '../icons';
 
+const iconContainerClassName = 'flex items-center gap-2';
+const skillTitleClassName =
+  'text-pretty text-lg font-semibold tracking-tight text-gray-900 sm:text-base';
+const iconSize = 30;
+
+const skillIconDictionary: Record<string, JSX.Element> = {
+  'Microsoft Office': (
+    <MicrosoftOfficeIcon width={iconSize} height={iconSize} />
+  ),
+  'Adobe Illustrator': (
+    <AdobeIllustratorIcon width={iconSize} height={iconSize} />
+  ),
+  'Adobe Photoshop': <AdobePhotoshopIcon width={iconSize} height={iconSize} />,
+  'Adobe InDesign': <AdobeInDesignIcon width={iconSize} height={iconSize} />,
+};
+
+const skillSetNameList = [
+  'Microsoft Office',
+  'Adobe Illustrator',
+  'Adobe Photoshop',
+  'Adobe InDesign',
+];
+
 export const SkillSet = () => {
   return (
     <div className="flex w-full flex-col">
-      <h5 className="text-pretty text-xl font-semibold tracking-tight text-gray-900 sm:text-2xl">
+      <h5 className="mt-2 text-pretty text-xl font-semibold tracking-tight text-gray-900">
         Skillset
       </h5>
-      <div className="xs:grid-cols-2 grid grid-cols-1 gap-6 py-6">
-        <div className="flex items-center gap-2">
-          <MicrosoftOfficeIcon />
-          <h6 className="text-pretty text-lg font-semibold tracking-tight text-gray-900 sm:text-xl">
-            Microsoft Office
-          </h6>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <AdobeIllustratorIcon />
-          <h6 className="text-pretty text-lg font-semibold tracking-tight text-gray-900 sm:text-xl">
-            Adobe Illustrator
-          </h6>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <AdobePhotoshopIcon />
-          <h6 className="text-pretty text-lg font-semibold tracking-tight text-gray-900 sm:text-xl">
-            Adobe Photoshop
-          </h6>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <AdobeInDesignIcon />
-          <h6 className="text-pretty text-lg font-semibold tracking-tight text-gray-900 sm:text-xl">
-            Adobe InDesign
-          </h6>
-        </div>
+      <div className="grid grid-cols-1 gap-6 py-6 xs:grid-cols-2">
+        {skillSetNameList.map((skillName) => (
+          <div className={iconContainerClassName} key={skillName}>
+            {skillIconDictionary[skillName]}
+            <h6 className={skillTitleClassName}>{skillName}</h6>
+          </div>
+        ))}
       </div>
     </div>
   );
