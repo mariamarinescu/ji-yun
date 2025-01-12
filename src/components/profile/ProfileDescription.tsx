c4a081c367b9b3aa323797706f2566aefd1382ca
Maria Marinescu <mariafmarinescu@outlook.com>
2025-01-12 16:04:28 +0100

    fix(profile): remove quotes from text

--- src/components/profile/ProfileDescription.tsx
+++ src/components/profile/ProfileDescription.tsx
@@ -1,16 +1,16 @@
 const textClassName = 'text-xs lg:text-base pl-1 text-gray-500';
-const quotesClassName = 'text-xl/10 lg:text-3xl/10 font-bold text-gray-900';
+// const quotesClassName = 'text-xl/10 lg:text-3xl/10 font-bold text-gray-900';
 export const ProfileDescription = () => {
   return (
     <>
       <p className="lgtext-xl/8 mt-2 text-start text-base/8 text-gray-700">
-        <span className={quotesClassName}>”</span>
+        {/* <span className={quotesClassName}>”</span> */}
         As a berlin-based brand designer, with a multicultural background and I
         blend organization and analytical thinking to craft impactful,
         culturally resonant brand strategies with a focus on “diversity”. My
         work highlights identity, heritage and unity delivering meaningful brand
         experiences.
-        <span className={quotesClassName}>”</span>
+        {/* <span className={quotesClassName}>”</span> */}
       </p>
       <div className="flex h-fit w-full flex-col gap-4 py-4">
         <p className="ml-2 font-caveat text-3xl font-bold lg:text-4xl">
